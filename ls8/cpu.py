b55e088f79d90ba544fea9ac8a3fdb7c016656cc
dgbailey <bailey.dust@gmail.com>
2019-08-20 15:38:39 -0700

    MULT working with dynamic load

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,6 +6,7 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
+        
         """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
@@ -13,28 +14,37 @@ class CPU:
         self.HLT = 0b00000001
         self.LDI = 0b10000010
         self.PRN = 0b01000111
+        self.MUL = 0b10100010
         self.running = True
 
-    def load(self):
-        """Load a program into memory."""
+    def load(self,filename):
+        """Load a program into self.ram."""
+        
+     
+        try:
+            address = 0
+            with open(filename) as f:
+                for line in f:
+                    # Split before and after any comment symbols
+                    comment_split = line.split("#")
+                    num = comment_split[0].strip()
+                    # Ignore blanks
+                    if num == "":
+                        continue
 
-        address = 0
+                    value = int(num,2)
+                    self.ram[address] = value
+                    address += 1
 
-        # For now, we've just hardcoded a program:
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+            sys.exit(2)
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        if len(sys.argv) != 2:
+            print("usage: simple.py <filename>", file=sys.stderr)
+            sys.exit(1)
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+     
 
 
 
@@ -79,26 +89,32 @@ class CPU:
           
             try:
             #read next two instructions in case they are needed
-                operand_a = self.ram_read(self.pc + 1)
-                operand_b = self.ram_read(self.pc + 2)
+                instruction_a = self.ram_read(self.pc + 1)
+                instruction_b = self.ram_read(self.pc + 2)
 
             except IndexError:
-                print("register operand out of range")
+                print("register instruction out of range")
             
 
            
 
             if ir == self.LDI:
                 print("LDI")
-                reg_address = operand_a
-                data = operand_b
+                reg_address = instruction_a
+                data = instruction_b
                 self.reg_write(data,reg_address)
 
 
             elif ir == self.PRN:
                 print("PRINT")
-                data = self.reg_read(operand_a)
+                data = self.reg_read(instruction_a)
                 print(data)
+
+            elif ir == self.MUL:
+                register_a = self.reg_read(instruction_a)
+                register_b = self.reg_read(instruction_b)
+                self.reg_write(register_a*register_b,instruction_a)
+                print("MUL")
                 
 
             elif ir == self.HLT:
@@ -107,6 +123,7 @@ class CPU:
                 self.running = False
             
             #go to next instruction based of of two high bits of current instruction in IR
+            #you many need custom jump numbers based on OP CODE
             next_instruction = self.pc + ir >>6
             if next_instruction >= 1:
 
@@ -135,3 +152,8 @@ class CPU:
 
     def reg_write(self,rd,ra):
         self.reg[ra] = rd
+
+
+    #load CLI
+
+    
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -4,8 +4,8 @@
 
 import sys
 from cpu import *
-
+filepath = sys.argv[1]
 cpu = CPU()
 
-cpu.load()
+cpu.load(filepath)
 cpu.run()
